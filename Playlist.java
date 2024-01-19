ebb550f9b2b2a41ad870584194d08814451abefa
Jackson Brathwaite <jbrathwaite25@students.polytechnic.org>
2024-01-18 18:26:10 -0800

    Worked on Playlist.java and changed parameter order of constructor in Song.java

--- Playlist.java
+++ Playlist.java
@@ -4,22 +4,74 @@ import java.util.ArrayList;
  * The Playlist class, which will keep track of a playlist of Song objects
  * Refer to the project description to make sure you have access to all available methods
  */
-public class Playlist {
+public class Playlist{
     /**
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
-
-
-
+    ArrayList<Song> dopamine;
 
      /**
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
-
-
+      public Playlist()
+      {
+      dopamine = new ArrayList<Song>();
+      }
       
+      public void addSong(String name, String artist, double dur, boolean liked)
+      {
+        Song track = new Song(name, artist, dur, liked);
+        dopamine.add(track);
+      }
+
+      public ArrayList likedSongs()
+      {
+        ArrayList<Song> goodSongs = new ArrayList<Song>();
+        for(int i = 0; i < dopamine.size(); i++)
+        {
+            if(dopamine.get(i).isLiked())
+            {
+                goodSongs.add(dopamine.get(i));
+            }
+        }
+        return goodSongs;
+      }
+
+      public void like(String name)
+      {
+        for(int i = 0; i < dopamine.size(); i++)
+        {
+            if(dopamine.get(i).isLiked() == false && dopamine.get(i).getName().equals(name))
+            {
+                dopamine.get(i).like();
+            }
+        }
+      } 
+
+      public void removeSong(String name)
+      {
+        for(int i = 0; i < dopamine.size(); i++)
+        {
+            if(dopamine.get(i).getName().equals(name))
+            {
+                dopamine.remove(i);
+            }
+        }
+      }
+
+      public double totalDuration()
+      {
+        double sum = 0;
+        for(int i = 0; i < dopamine.size(); i++)
+        {
+            sum += dopamine.get(i).songDur();
+        }
+        return sum;
+      }
+
+
 
       /**
        * Methods-- Remember that you need to be able to complete all of the following:
